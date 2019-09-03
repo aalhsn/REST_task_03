9bd677186d49df22463bc99e10d4b04d3feb2298
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-09-03 14:26:52 +0300

    Abdullah Alhasan

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from .models import Flight, Booking
 
 
@@ -25,3 +24,4 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['date', 'passengers']
+
--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,7 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from datetime import datetime
-
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from .models import Flight, Booking
 from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer
-
+from datetime import datetime
 
 class FlightsList(ListAPIView):
 	queryset = Flight.objects.all()
@@ -33,3 +31,13 @@ class CancelBooking(DestroyAPIView):
 	queryset = Booking.objects.all()
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+
+class CreateBooking(CreateAPIView):
+	serializer_class = UpdateBookingSerializer
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user, flight_id=self.kwargs['flight_id'])
+
+
+
+
